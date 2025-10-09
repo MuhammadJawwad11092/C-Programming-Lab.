a9a5ae0fe40a1b0f2503abd6dd8199ebbf63a3de
MuhammadJawwad11092 <k250536@nu.edu.pk>
2025-10-09 22:09:45 +0500

    Update task9.c

--- PF LAB#6 ALL TASK/task9.c
+++ PF LAB#6 ALL TASK/task9.c
@@ -17,3 +17,21 @@ int main() {
                 largest = num;
             if (num < smallest)
                 smallest = num;
+        }
+
+        printf("Current largest: %d\n", largest);
+        printf("Current smallest: %d\n", smallest);
+
+        printf("Do you want to continue? (yes/no): ");
+        scanf("%s", choice);
+
+        if (strcmp(choice, "no") == 0)
+            break;
+    }
+
+    printf("\n--- Final Result ---\n");
+    printf("Largest number entered: %d\n", largest);
+    printf("Smallest number entered: %d\n", smallest);
+
+    return 0;
+}
